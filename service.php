185ef943238c6a8657c51ad3d43c5703d8806ba2
dengmatet <168043478+dengmatet@users.noreply.github.com>
2024-09-26 20:15:04 +0200

    title has been change

--- service.php
+++ service.php
@@ -21,7 +21,7 @@ if(isset($_GET['id'])){
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Websites WWW.Services.com | DengCode</title>
+    <title>DeShop Services.com</title>
     <link rel="stylesheet" href="style.css">
     <style>
         /* section for top class and table row*/
--- view.php
+++ view.php
@@ -3,7 +3,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Website WWW.index.com | Deng code</title>
+    <title>DeShop View.com</title>
     <link rel="stylesheet" href="style.css">
 </head>
 <style>
