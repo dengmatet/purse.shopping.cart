5f2f2667eece8266529d30e6a9c6bb0dc834523a
dengmatet <168043478+dengmatet@users.noreply.github.com>
2024-10-01 01:06:53 +0200

    changing some title

--- view.php
+++ view.php
@@ -3,7 +3,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>DeShop View.com</title>
+    <title>DeShop View Shopping.com</title>
     <link rel="stylesheet" href="style.css">
 </head>
 <style>
